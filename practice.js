8c5c9320ba63229c51cc069d363296008291d5cb
JealousMarc <onionator@gmail.com>
2015-07-01 19:48:18 -0700

    July 1 Practice
    A description of how not knowing how to type will lead you to be mauled
    by a bobcat.

---
+++ practice.js
@@ -0,0 +1,20 @@
+/*
+function friend(name, link) {
+	document.writeln('<a href="' + link + '">' + name + '</a>');
+}
+	
+
+/* jquery 
+  $(function() {
+    $( "#slider" ).slider();
+  });
+
+
+
+$(document).ready(function(){
+    $(".picture").click(function(){
+        $("div").animate({left: '250px'});
+    });
+});
+
+*/
